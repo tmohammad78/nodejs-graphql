e1fe442e8c5825b8baa9cdbcdc328b93b111d64e
mohammad <mohammadtaheri506@gmail.com>
2020-03-28 19:22:44 +0430

    add validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,29 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+
+const validator = require("validator");
 module.exports = {
   createUser: async function({ userInput }, req) {
     // const email = userInput.email;
+    const error = [];
+    if (!validator.isEmail(userInput.email)) {
+      error.push({
+        message: "Error is invalid"
+      });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      validator.isLength(userInput.password, { min: 5 })
+    ) {
+      error.push({
+        message: "pass is short"
+      });
+    }
+
+    if (error.length > 0) {
+      const error = new Error("invalid type input");
+      throw error;
+    }
     const existngUser = await User.findOne({ email: userInput.email });
     if (existngUser) {
       const error = new Error("User exist already");
